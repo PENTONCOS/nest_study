f49e2626fded1705197a126a871896389ad71b0c
jiapandong <jiapandong@supconit.com>
2023-06-01 15:04:58 +0800

    feat: 通过form data 上传图片

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors, UploadedFiles } from '@nestjs/common';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { PersonService } from './person.service';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
@@ -8,13 +9,7 @@ export class PersonController {
   constructor(private readonly personService: PersonService) { }
 
 
-
-  @Get()
-  findAll() {
-    return this.personService.findAll();
-  }
-
-  // 1. url query请求方式 
+  // 1. url query
   // 通过@Query(参数名)取出注入到controller
   // 注意，这个 find 的路由要放到 :id 的路由前面，因为 Nest 是从上往下匹配的，如果放在后面，那就匹配到 :id 的路由了
   @Get('find')
@@ -23,7 +18,7 @@ export class PersonController {
 
   }
 
-  // 2. url param请求方式 
+  // 2. url param
   // 通过@Param(参数名)取出注入到controller
   // @Controller('api/person') 的路由和 @Get(':id') 的路由会拼到一起，也就是只有 /api/person/xxx 的 get 请求才会走到这个方法。
   @Get(':id')
@@ -31,7 +26,8 @@ export class PersonController {
     return `received: id=${id}`
   }
 
-  // 3. form urlencoded / json 请求方式
+  // 3. form urlencoded
+  // 4. json
   // 用 Nest 接收的话，使用 @Body 装饰器，Nest 会解析请求体，然后注入到 dto 中。
   // dto 是 data transfer object，就是用于封装传输的数据的对象
   @Post()
@@ -40,6 +36,23 @@ export class PersonController {
     return `received: ${JSON.stringify(createPersonDto)}`;
   }
 
+  // 5. form data
+  // Nest 解析 form data 使用 FilesInterceptor 的拦截器，用 @UseInterceptors 装饰器启用，然后通过 @UploadedFiles 来取。非文件的内容，同样是通过 @Body 来取。
+  @Post('file')
+  @UseInterceptors(AnyFilesInterceptor({
+    dest: 'uploads/', // 上传后文件所在路径
+  }))
+  body(@Body() createPersonDto: CreatePersonDto, @UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log(files);
+    return `received: ${JSON.stringify(createPersonDto)}`
+  }
+
+
+
+  @Get()
+  findAll() {
+    return this.personService.findAll();
+  }
 
   // @Get(':id')
   // findOne(@Param('id') id: string) {
